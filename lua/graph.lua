c6bae418dd759077b71134ba7bf56a0393646bba
CoderPuppy <coderpup@gmail.com>
2016-09-27 01:42:01 -0400

    Label the start and end in graphs

--- lua/graph.lua
+++ lua/graph.lua
@@ -30,5 +30,8 @@ return function(instrs, labels)
 		out = out .. ('%q -> "instr-%d";\n'):format('lbl-' .. lbl, i)
 	end
 
+	out = out .. 'START -> "instr-1";\n'
+	out = out .. ('"instr-%d" [label=END];\n'):format(#instrs + 1)
+
 	return out
 end
