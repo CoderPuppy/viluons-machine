87fcde9891ff51ccdbf35b335dc34bb8d6bee869
CoderPuppy <coderpup@gmail.com>
2016-09-27 01:27:44 -0400

    Generate control flow graphs

---
+++ lua/bin/graph.lua
@@ -0,0 +1,48 @@
+local require = require 'nsrq' ()
+
+local parse = require '../parse'
+local passes = require '../passes'
+local graph = require '../graph'
+local args = require '../args'
+
+local file_arg = {}
+local passes_arg = {}
+
+args.parse({...}, args.merge(
+	args.one(file_arg),
+	args.pattern('^%-p(.+)$', args.map(function(arg)
+		local name, parg = arg[1]:match '^([^%(]+)(%b())$'
+		if name then
+			parg = parg:sub(2, -2)
+		else
+			name = arg[1]
+		end
+		
+		if passes[name] then
+			return {passes[name], parg}
+		else
+			error(('unknown pass: %q'):format(name))
+		end
+	end, args.collect(passes_arg)))
+))
+
+if not file_arg.set then
+	error('no file specified')
+end
+
+local instrs, labels
+do
+	local h = io.open(file_arg.v, 'r')
+	if not h then error(('No file: %q'):format(file_arg.v), 0) end
+	local contents = h:read '*a'
+	h:close()
+	instrs, labels = parse(contents)
+end
+
+for _, pass in ipairs(passes_arg) do
+	instrs, labels = pass[1](pass[2], instrs, labels)
+end
+
+print('digraph {')
+io.write(graph(instrs, labels))
+print('}')
--- lua/graph.lua
+++ lua/graph.lua
@@ -2,32 +2,32 @@ return function(instrs, labels)
 	local out = ''
 
 	for i, instr in ipairs(instrs) do
-		out = out .. ('instr-%d ['):format(i)
+		out = out .. ('"instr-%d" ['):format(i)
 		if instr[1] == 'JMP' then
 			out = out .. ('label=JMP];\n'):format()
 			if type(instr[2]) == 'number' then
-				out = out .. ('instr-%d -> instr-%d [];\n'):format(i, instr[2])
+				out = out .. ('"instr-%d" -> "instr-%d" [];\n'):format(i, i + instr[2] + 1)
 			end
 		elseif instr[1] == 'JIZ' then
 			out = out .. ('label="JIZ %d"];\n'):format(instr[2])
 			if type(instr[3]) == 'number' then
-				out = out .. ('instr-%d -> instr-%d [label=%q];\n'):format(i, instr[3], tostring(instr[2]) .. ' == 0')
+				out = out .. ('"instr-%d" -> "instr-%d" [label=%q];\n'):format(i, i + instr[3] + 1, ('K%d == 0'):format(instr[2]))
 			end
-			out = out .. ('instr-%d -> instr-%d [];\n'):format(i, i + 1)
+			out = out .. ('"instr-%d" -> "instr-%d" [];\n'):format(i, i + 1)
 		elseif instr[1] == 'INCR' then
-			out = out .. 'label="INCR ' .. tostring(instr[2]) .. '"];\n'
-			out = out .. ('instr-%d -> instr-%d [];\n'):format(i, i + 1)
+			out = out .. ('label="INCR K%d"];\n'):format(instr[2])
+			out = out .. ('"instr-%d" -> "instr-%d" [];\n'):format(i, i + 1)
 		elseif instr[1] == 'DECR' then
-			out = out .. ('label="DECR %d"];\n'):format(instr[2])
-			out = out .. ('instr-%d -> instr-%d [];\n'):format(i, i + 1)
+			out = out .. ('label="DECR K%d"];\n'):format(instr[2])
+			out = out .. ('"instr-%d" -> "instr-%d" [];\n'):format(i, i + 1)
 		else
 			error('unknown instr: ' .. tostring(instr[1]))
 		end
 	end
 
-	for lbl, i in ipairs(labels) do
+	for lbl, i in pairs(labels) do
 		out = out .. ('%q [label=%q];\n'):format('lbl-' .. lbl, lbl)
-		out = out .. ('%q -> instr-%d;\n'):format('lbl-' .. lbl, i)
+		out = out .. ('%q -> "instr-%d";\n'):format('lbl-' .. lbl, i)
 	end
 
 	return out
